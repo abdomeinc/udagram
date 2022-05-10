ca6addb0ea65f22774b5bad7e65d05cc69b070fb
Ali Al-Masry <abdo.me.inc@gmail.com>
2022-05-10 10:50:11 +0200

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,5 @@
+eb init egyfwd-udagram-api --platform node.js --region us-east-1
+eb create --sample egyfwd-udagram-api-dev
+eb use egyfwd-udagram-api-dev
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB PORT=$PORT DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
 eb deploy egyfwd-udagram-api-dev
